764502dc65d49b81f30ed7cedda2c72b45df4f75
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-17 09:26:21 +0530

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Bhavin Bhadani on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
